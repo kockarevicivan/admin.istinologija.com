c09f8cca14f6f5926f5cea50e74a81eaf98c3e43
Aleksandar Tisma <aleksandar.tisma@marbleit.rs>
2022-10-26 14:46:17 +0000

    Add public url

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+	url: "https://admin.istinologija.com",
   app: {
     keys: env.array('APP_KEYS', ['dfsjo23u89dfkh289u298']),
   },
